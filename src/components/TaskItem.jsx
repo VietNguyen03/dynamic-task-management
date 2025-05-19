a659742c050d09a9dbc02b420adfbcae94028c2d
Viet Nguyen <vietnguyen@Viets-MacBook-Pro.local>
2025-05-19 16:38:00 -0400

    Fix trash logo

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -15,7 +15,7 @@ const TaskItem = ({ task, toggleComplete, deleteTask }) => {
           {task.text} ({task.priority.charAt(0).toUpperCase() + task.priority.slice(1)})
         </span>
       </div>
-      <button onClick={() => deleteTask(task.id)} className="bg-red-500 text-white text-xl rounded-full p-2 hover:bg-red-600 transition"
+      <button onClick={() => deleteTask(task.id)} className="bg-red-500 text-white text-xl rounded-full p-2 hover:bg-red-600 border-red-700 shadow"
       title="Delete Task"
 >
   🗑️
